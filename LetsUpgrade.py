74ab113c72ac6d61aa52bac090f9f8ac837f221a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-10-01 19:01:32 +0530

    Update LetsUpgrade.py

--- LetsUpgrade.py
+++ LetsUpgrade.py
@@ -1,2 +1,4 @@
 $ print ('This is My LetsUpgrade Assignments - PYTHON - Zero to Hero')
+print("Under this Bootcamp I'll develope my own python skills")
+print("By great mentor Mr. Sai Sondarkar Sir")
 
